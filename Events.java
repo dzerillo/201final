5a3afff99136693530e29fa14ee70a9619f980c8
hongyun1021 <54826418+hongyun1021@users.noreply.github.com>
2019-11-26 22:42:23 -0600

    Add files via upload

---
+++ Events.java
@@ -0,0 +1,28 @@
+
+public class Events {
+	
+	String event;
+	int eventValue;
+	
+	public Events (String event, int eventValue) {
+		this.event = event;
+		this.eventValue = eventValue;
+	}
+
+	public String getEvent() {
+		return event;
+	}
+
+	public void setEvent(String event) {
+		this.event = event;
+	}
+
+	public int getEventValue() {
+		return eventValue;
+	}
+
+	public void setEventValue(int eventValue) {
+		this.eventValue = eventValue;
+	}
+
+}
---
+++ Summary.java
@@ -0,0 +1,20 @@
+
+public class Summary {
+	String studentName;
+	String[] arrayEvents;
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String[] getArrayEvents() {
+		return arrayEvents;
+	}
+	public void setArrayEvents(String[] arrayEvents) {
+		this.arrayEvents = arrayEvents;
+	}
+	
+
+}
